d7c9357add8e868fc3583c7e993d4968ccbb7e9c
fonhorst <fonhorst@alipoov.nb@gmail.com>
2021-07-08 17:11:40 +0300

    added configurations for irace

---
+++ src/run-irace.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -ex
+
+dir=$1
+
+shift 1
+
+log_file="/var/lib/irace/irace-log.Rdata"
+recovery_file="/var/lib/irace/irace-backup.Rdata"
+
+if [[ -f "${log_file}" ]]; then
+    echo "${log_file} exists. Make it as a recovery file ${recovery_file}"
+    mv "${log_file}" "${recovery_file}"
+    args="--recovery-file ${recovery_file} --log-file ${log_file}"
+elif [[ -f "${recovery_file}" ]]; then
+    echo "No ${log_file} found. But recovery file ${recovery_file} exists. The run will continue from it."
+    args="--recovery-file ${recovery_file} --log-file ${log_file}"
+else
+    args="--log-file ${log_file}"
+fi
+
+cd "${dir}" || exit 1
+exec "$IRACE_HOME"/bin/irace "${args}" "${@}"
