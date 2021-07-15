4fd8a161ec28c61a2e10cab0d6da3e10dec235f1
fonhorst <fonhorst@alipoov.nb@gmail.com>
2021-07-15 12:41:27 +0300

    added scripts for starting and stopping repeater and the real config

---
+++ bin/stop-repeater.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [[ -z "${KUBE_NAMESPACE}" ]]
+then
+  kubectl_args=""
+else
+  kubectl_args="-n ${KUBE_NAMESPACE}"
+fi
+
+exec kubectl ${kubectl_args} delete -f conf/repeater-job.yaml
