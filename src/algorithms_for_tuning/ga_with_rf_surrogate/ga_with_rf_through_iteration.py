63c579054bbaf673a84065b2f45385c726ed7e59
JessikaSmith <mariyaxod@yandex.ru>
2021-07-16 18:55:35 +0300

    Add random forest configuration

--- src/algorithms_for_tuning/ga_with_rf_surrogate/ga_with_rf_through_iteration.py
+++ src/algorithms_for_tuning/ga_with_rf_surrogate/ga_with_rf_through_iteration.py
@@ -11,17 +11,13 @@ from yaml import Loader
 import copy
 import random
 from typing import List
-
-from algorithms_for_tuning.genetic_algorithm.mutation import mutation
-from algorithms_for_tuning.genetic_algorithm.crossover import crossover
-from algorithms_for_tuning.genetic_algorithm.selection import selection
+from algorithms_for_tuning.genetic_algorithm.ga import GA
 from algorithms_for_tuning.utils import make_log_config_dict
+from kube_fitness.tasks import IndividualDTO, TqdmToLogger
 
 warnings.filterwarnings("ignore")
 
-from kube_fitness.tasks import IndividualDTO, TqdmToLogger
-
-logger = logging.getLogger("GA")
+logger = logging.getLogger("GA_surrogate")
 
 # getting config vars
 if "FITNESS_CONFIG_PATH" in os.environ:
@@ -62,7 +58,7 @@ else:
     def log_best_solution(individual: IndividualDTO):
         pass
 
-# TODO: check types correctness
+# TODO: check types correctness & None processing
 NUM_FITNESS_EVALUATIONS = config['globalAlgoParams']['numEvals']
 NUM_INDIVIDUALS = config['globalAlgoParams']['numIndividuals']
 MUTATION_TYPE = config['globalAlgoParams']['mutationType']
@@ -75,26 +71,50 @@ ELEM_CROSS_PROB = config['globalAlgoParams']['elemCrossProb']
 
 @click.command(context_settings=dict(allow_extra_args=True))
 @click.option('--dataset', required=True, help='dataset name in the config')
-
 @click.option('--log-file', default="/var/log/tm-alg.log",
               help='a log file to write logs of the algorithm execution to')
 @click.option('--exp-id', required=True, type=int, help='mlflow experiment id')
-def run_algorithm(dataset,
-                  num_individuals,
-                  mutation_type, crossover_type, selection_type,
-                  elem_cross_prob, cross_alpha,
-                  best_proc, log_file, exp_id):
+@click.option('--surrogate-name', default=None, type=str,
+              help="Enter surrogate name if you want to start calculations with surrogate")
+@click.option('--rf-n-estimators', type=int)
+@click.option('--rf-n-criterion', type=str)
+@click.option('--rf-max-depth', type=int)
+@click.option('--rf-min-samples-split', type=float)
+@click.option('--rf-min-samples-leaf', type=float)
+@click.option('--rf-min-weight-fraction-leaf', type=float)
+@click.option('--rf-max-features', type=float)
+@click.option('--rf-oob-score', type=bool)
+@click.option('--rf-n-jobs', type=int)
+def run_algorithm(dataset, log_file, exp_id, surrogate_name,
+                  rf_n_estimators, rf_criterion, rf_max_depth, rf_min_samples_split,
+                  rf_min_samples_leaf, rf_min_weight_fraction_leaf, rf_max_features, rf_oob_score, rf_n_jobs,
+
+                  ):
     run_uid = uuid.uuid4() if not config['testMode'] else None
     logging_config = make_log_config_dict(filename=log_file, uid=run_uid)
     logging.config.dictConfig(logging_config)
 
+    kwargs = dict()
+    if surrogate_name=='random-forest-regressor':
+        kwargs = {
+                  'n_estimators': rf_n_estimators,
+                  'criterion': rf_criterion,
+                  'max_depth': rf_max_depth,
+                  'min_samples_split': rf_min_samples_split,
+                  'min_samples_leaf': rf_min_samples_leaf,
+                  'min_weight_fraction_leaf': rf_min_weight_fraction_leaf,
+                  'max_features': rf_max_features,
+                  'oob_score': rf_oob_score,
+                  'n_jobs': rf_n_jobs
+                  }
+
     logger.info(f"Starting a new run of algorithm. Args: {sys.argv[1:]}")
 
-    if elem_cross_prob is not None:
-        elem_cross_prob = float(elem_cross_prob)
+    if ELEM_CROSS_PROB is not None:
+        elem_cross_prob = float(ELEM_CROSS_PROB)
 
-    if cross_alpha is not None:
-        cross_alpha = float(cross_alpha)
+    if CROSS_ALPHA is not None:
+        cross_alpha = float(CROSS_ALPHA)
 
     g = GA(dataset=dataset,
            num_individuals=NUM_INDIVIDUALS,
@@ -106,9 +126,7 @@ def run_algorithm(dataset,
            num_fitness_evaluations=NUM_FITNESS_EVALUATIONS,
            best_proc=BEST_PROC,
            alpha=CROSS_ALPHA,
-           exp_id=exp_id)
+           exp_id=exp_id,
+           **kwargs)
     best_value = g.run(verbose=True)
     print(best_value * (-1))
-
-
-# class GA
