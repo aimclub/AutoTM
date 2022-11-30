4ec0b09c0ee8cced96a82bcccaeed09b9e60d69c
ngc436 <mariyaxod@yandex.ru>
2022-12-01 01:16:07 +0300

    Update build

--- examples/full_pipeline_example.py
+++ examples/full_pipeline_example.py
@@ -7,23 +7,23 @@ from autotm.preprocessing.dictionaries_preparation import prepare_all_artifacts
 from autotm.algorithms_for_tuning.genetic_algorithm.genetic_algorithm import run_algorithm
 
 PATH_TO_DATASET = '../data/sample_corpora/sample_dataset_lenta.csv'
-SAVE_PATH = '../data/processed_sample_corpora' # place where all the artifacts will be stored
+SAVE_PATH = '../data/processed_sample_corpora'  # place where all the artifacts will be stored
 
-dataset = pd.read_csv(PATH_TO_DATASET)
-lang = 'ru' # available languages: ru, en
+dataset = pd.read_csv(PATH_TO_DATASET) # dataset with corpora to be processed
 col_to_process = 'text'
-min_tokens_num = 3
+lang = 'ru'  # available languages: ru, en
+min_tokens_num = 3 # the minimal amount of tokens after processing to save the result
 
 if __name__ == '__main__':
     print('Stage 1: Dataset preparation')
-    # process_dataset(PATH_TO_DATASET, col_to_process, SAVE_PATH,
-    #                 lang, min_tokens_count=min_tokens_num)
+    process_dataset(PATH_TO_DATASET, col_to_process, SAVE_PATH,
+                    lang, min_tokens_count=min_tokens_num)
 
-    # prepare_all_artifacts(SAVE_PATH)
+    prepare_all_artifacts(SAVE_PATH)
 
     print('Stage 2: Tuning the topic model')
 
-    # SAVE_PATH,
+    # exp_id and dataset_name will be needed further to store results in mlflow
     topics = run_algorithm(data_path=SAVE_PATH,
                            exp_id=1,
                            dataset='test',
