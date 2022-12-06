83c9b0806ca56ec92568bd76e2b52ad9b29365d0
JessikaSmith <mariyaxod@yandex.ru>
2022-12-06 19:18:09 +0300

    Add run_name print

--- examples/full_pipeline_example.py
+++ examples/full_pipeline_example.py
@@ -2,38 +2,45 @@
 import os
 import pandas as pd
 import sys
+from autotm.infer import process_experiment
 
 from autotm.preprocessing.text_preprocessing import process_dataset
 from autotm.preprocessing.dictionaries_preparation import prepare_all_artifacts
 from autotm.algorithms_for_tuning.genetic_algorithm.genetic_algorithm import run_algorithm
 
-PATH_TO_DATASET = '../data/sample_corpora/sample_dataset_lenta.csv' # dataset with corpora to be processed
+PATH_TO_DATASET = '../data/sample_corpora/sample_dataset_lenta.csv'  # dataset with corpora to be processed
 SAVE_PATH = '../data/processed_sample_corpora'  # place where all the artifacts will be stored
 
 dataset = pd.read_csv(PATH_TO_DATASET)
 col_to_process = 'text'
 lang = 'ru'  # available languages: ru, en
-min_tokens_num = 3 # the minimal amount of tokens after processing to save the result
+min_tokens_num = 3  # the minimal amount of tokens after processing to save the result
+
+exp_id = 2
 
 if __name__ == '__main__':
-    print('Stage 1: Dataset preparation')
-    process_dataset(PATH_TO_DATASET, col_to_process, SAVE_PATH,
-                    lang, min_tokens_count=min_tokens_num)
-
-    prepare_all_artifacts(SAVE_PATH)
-
-    print('Stage 2: Tuning the topic model')
-
-    # exp_id and dataset_name will be needed further to store results in mlflow
-    best_result = run_algorithm(data_path=SAVE_PATH,
-                           exp_id=1,
-                           dataset='test',
-                           topic_count=3,
-                           log_file='./log_file_test.txt',
-                           num_iterations=2
-                           )
+    # print('Stage 1: Dataset preparation')
+    # process_dataset(PATH_TO_DATASET, col_to_process, SAVE_PATH,
+    #                 lang, min_tokens_count=min_tokens_num)
+    #
+    # prepare_all_artifacts(SAVE_PATH)
+    #
+    # print('Stage 2: Tuning the topic model')
+    #
+    # # exp_id and dataset_name will be needed further to store results in mlflow
+    # best_result, experiment_id = run_algorithm(data_path=SAVE_PATH,
+    #                             exp_id=exp_id,
+    #                             dataset='test',
+    #                             topic_count=3,
+    #                             log_file='./log_file_test.txt',
+    #                             num_iterations=2
+    #                             )
 
     # results of the run are stored in ./mlruns folder, experiment id is 'experiment_<exp_id>'
 
     print('Step 3: Looking at results and making inference')
+    PATH_TO_MLFLOW_RESULTS = './mlflow'
+    TEST_RUN_NAME = 'fitness-test-2ad5fbf9-45d6-4dfd-9b07-283d08ebb0e6'  # for now you can see the run name in console after each run
+
     # usage and inference
+    process_experiment(PATH_TO_MLFLOW_RESULTS)
