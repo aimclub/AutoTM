2f871f3f90b9e2b8b3a02d47cad40a6250ed89e9
ngc436 <mariyaxod@yandex.ru>
2023-01-28 02:17:52 +0300

    Update NM

--- examples/nelder_mead_experiments.py
+++ examples/nelder_mead_experiments.py
@@ -3,27 +3,29 @@ import time
 import pandas as pd
 import os
 
-DATA_PATH = '../data/experiment_datasets/20newsgroups_sample'
+fnames = ['20newsgroups_sample', 'amazon_food_sample', 'banners_sample', 'hotel-reviews_sample', 'lenta_ru_sample']
+data_lang = ['en', 'en', 'ru', 'en', 'ru']
+dataset_id = 1
+
+DATA_PATH = os.path.join('/ess_data/GOTM/datasets_TM_scoring', fnames[dataset_id])
 
 PATH_TO_DATASET = os.path.join(DATA_PATH, 'dataset_processed.csv')  # dataset with corpora to be processed
-SAVE_PATH = DATA_PATH # place where all the artifacts will be stored
-
+SAVE_PATH = DATA_PATH  # place where all the artifacts will be stored
 
 dataset = pd.read_csv(PATH_TO_DATASET)
-col_to_process = 'text'
-dataset_name = 'sample_20ng'
-lang = 'en'  # available languages: ru, en
+dataset_name = fnames[dataset_id] + '20ng_sample_with_nm'
+lang = data_lang[dataset_id]  # available languages: ru, en
 min_tokens_num = 3  # the minimal amount of tokens after processing to save the result
-num_iterations = 15
-topic_count = 50
+num_iterations = 20
+topic_count = 10
 exp_id = int(time.time())
 print(exp_id)
 
 if __name__ == '__main__':
     nelder_opt = NelderMeadOptimization(data_path=SAVE_PATH,
-                                dataset=dataset_name,
-                                exp_id=exp_id,
-                                topic_count=topic_count)
+                                        dataset=dataset_name,
+                                        exp_id=exp_id,
+                                        topic_count=topic_count)
 
     res = nelder_opt.run_algorithm(num_iterations=num_iterations)
     print(res)
