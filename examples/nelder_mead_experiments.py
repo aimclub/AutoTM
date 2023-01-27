f930ca205e871fe7738dfb5d28d84d85876481c9
ngc436 <mariyaxod@yandex.ru>
2023-01-28 02:49:50 +0300

    .

--- examples/nelder_mead_experiments.py
+++ examples/nelder_mead_experiments.py
@@ -2,10 +2,11 @@ from autotm.algorithms_for_tuning.nelder_mead_optimization.nelder_mead import Ne
 import time
 import pandas as pd
 import os
+# 2
 
 fnames = ['20newsgroups_sample', 'amazon_food_sample', 'banners_sample', 'hotel-reviews_sample', 'lenta_ru_sample']
 data_lang = ['en', 'en', 'ru', 'en', 'ru']
-dataset_id = 1
+dataset_id = 3
 
 DATA_PATH = os.path.join('/ess_data/GOTM/datasets_TM_scoring', fnames[dataset_id])
 
@@ -13,7 +14,7 @@ PATH_TO_DATASET = os.path.join(DATA_PATH, 'dataset_processed.csv')  # dataset wi
 SAVE_PATH = DATA_PATH  # place where all the artifacts will be stored
 
 dataset = pd.read_csv(PATH_TO_DATASET)
-dataset_name = fnames[dataset_id] + '20ng_sample_with_nm'
+dataset_name = fnames[dataset_id] + 'sample_with_nm'
 lang = data_lang[dataset_id]  # available languages: ru, en
 min_tokens_num = 3  # the minimal amount of tokens after processing to save the result
 num_iterations = 20
