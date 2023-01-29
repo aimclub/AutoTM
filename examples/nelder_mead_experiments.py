7979792a43c53f0913778d7ba5a275906382c279
ngc436 <mariyaxod@yandex.ru>
2023-01-29 23:51:55 +0300

    Add structure

--- examples/nelder_mead_experiments.py
+++ examples/nelder_mead_experiments.py
@@ -7,7 +7,7 @@ import os
 
 fnames = ['20newsgroups_sample', 'amazon_food_sample', 'banners_sample', 'hotel-reviews_sample', 'lenta_ru_sample']
 data_lang = ['en', 'en', 'ru', 'en', 'ru']
-dataset_id = 2
+dataset_id = 0
 
 DATA_PATH = os.path.join('/ess_data/GOTM/datasets_TM_scoring', fnames[dataset_id])
 
@@ -18,7 +18,7 @@ dataset = pd.read_csv(PATH_TO_DATASET)
 dataset_name = fnames[dataset_id] + 'sample_with_nm'
 lang = data_lang[dataset_id]  # available languages: ru, en
 min_tokens_num = 3  # the minimal amount of tokens after processing to save the result
-num_iterations = 20
+num_iterations = 200
 topic_count = 10
 exp_id = int(time.time())
 print(exp_id)
