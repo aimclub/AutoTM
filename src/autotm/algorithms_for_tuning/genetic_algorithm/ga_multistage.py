9bc05d94ec53f7fcf265941283b1e386779335b1
JessikaSmith <mariyaxod@yandex.ru>
2023-02-01 17:46:45 +0300

    Add CMA-ES strategy from DEAP

---
+++ src/autotm/algorithms_for_tuning/genetic_algorithm/ga_multistage.py
@@ -0,0 +1,24 @@
+import deap
+
+class ModelIn
+
+class ModelStorage:
+    def __init__(self):
+        self.stage_1_components = {} # {config_id: id}
+        self.stage_1_hyperp = {} # {config_id: [[params1, params2]]}
+
+    def model_search(self, model):
+
+        for model.components
+
+
+class GA:
+    def __init__(self, max_stages): # max_stage_len
+        self.max_stages = max_stages# amount of unique regularizers
+        raise NotImplementedError
+
+    def init_individ(self):
+        raise NotImplementedError
+
+    def ffff(self):
+        raise NotImplementedError
