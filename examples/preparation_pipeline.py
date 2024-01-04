7ca5f2cc84893c97db1b5ba239bcc8b925790e62
Nikolay <184880@niuitmo.ru>
2024-01-04 18:34:32 +0300

    Feature/cooc dict (#21)
    * add optimized version of co occurence computations (cooc) with no parallelism
    
    * fix for  vocabulary file computations (saved correct order of words) 
    
    * add pytest ini options into pyproject.toml to correctly identify rootpath via pytestconfig.rootpath
    
    ---------
    
    Co-authored-by: fonhorst <fonhorst@alipoov.nb@gmail.com>

---
+++ examples/preparation_pipeline.py
@@ -0,0 +1,37 @@
+import logging
+import os.path
+
+from autotm.preprocessing import PREPOCESSED_DATASET_FILENAME
+from autotm.preprocessing.dictionaries_preparation import prepare_all_artifacts, prepearing_cooc_dict
+from autotm.preprocessing.text_preprocessing import process_dataset
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
+
+
+PATH_TO_DATASET = "../data/sample_corpora/sample_dataset_lenta.csv"  # dataset with corpora to be processed
+SAVE_PATH = "../data/processed_sample_corpora"  # place where all the artifacts will be stored
+
+col_to_process = "text"
+dataset_name = "lenta_sample"
+lang = "ru"  # available languages: ru, en
+min_tokens_num = 3  # the minimal amount of tokens after processing to save the result
+
+
+if __name__ == "__main__":
+    logger.info("Stage 1: Dataset preparation")
+    if not os.path.exists(SAVE_PATH):
+        process_dataset(
+            PATH_TO_DATASET,
+            col_to_process,
+            SAVE_PATH,
+            lang,
+            min_tokens_count=min_tokens_num,
+        )
+    else:
+        logger.info("The preprocessed dataset already exists. Found files on path: %s" % SAVE_PATH)
+
+    logger.info("Stage 2: Prepare all artefacts")
+    prepare_all_artifacts(SAVE_PATH)
+
+    logger.info("All finished")
