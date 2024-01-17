7988535b658d9817b5284e3712b4e5d53d05ba9f
ngc436 <mariyaxod@yandex.ru>
2024-01-17 17:23:02 +0300

    Update example

--- examples/graph_profile_example.py
+++ examples/graph_profile_example.py
@@ -8,15 +8,18 @@ from datasets import load_dataset
 from sklearn.model_selection import train_test_split
 from autotm.base import AutoTM
 from autotm.ontology.ontology_extractor import build_graph
+from autotm.clustering import cluster_phi
 import networkx as nx
 
 
 df = load_dataset('zloelias/lenta-ru')  # https://huggingface.co/datasets
-text_sample = df['train']['text'][:5000]
+text_sample = df['train']['text']
 df = pd.DataFrame({'text': text_sample})
 
 working_dir_path = 'autotm_artifacts'
 
+# Инициализируем модель и укажем наименование
+# суррогатной модели для ускорения вычислений
 autotm = AutoTM(
         topic_count=20,
         texts_column_name='text',
@@ -25,13 +28,30 @@ autotm = AutoTM(
         },
         # alg_name='ga',
         alg_params={
-            "num_iterations": 10,
+            "num_iterations": 15,
+            "surrogate_alg_name": "GPR"
         },
         working_dir_path=working_dir_path
     )
 
 mixtures = autotm.fit_predict(df)
 
+# извлекаем матрицу phi - распределение слов по темам
+phi_df = autotm._model.get_phi()
+
+# проведем кластеризацию полученных данных
+# из которой можно увидеть, что достаточно хорошо выделяются кластера
+cluster_phi(phi_df, n_clusters=10, plot_img=True)
+
+# осмотрим, какие документы есть в кластерах
+mixtures['labels'] = y_kmeans.labels_
+res_df = df.join(mixtures)
+print(res_df[res_df['labels'] == 8].text.tolist()[:3])
+
+# займемся построением графа, как мы уже знаем
+# для лучшей интерпретируемости лучше сделать
+# словарь названий тем
+autotm.print_topics()
 labels_dict = {'main0':'Устройства',
                'main1': 'Спорт',
                'main2':'Экономика',
@@ -56,9 +76,49 @@ labels_dict = {'main0':'Устройства',
 
 res_dict, nodes = build_graph(autotm, topic_labels=labels_dict)
 
-# visualizing the graph
+# посмотрим на результат обработки - словарь со связями
+print(res_dict)
+
+# Визуализируем получаемый граф
 g = nx.DiGraph()
 g.add_nodes_from(nodes)
 for k, v in res_dict.items():
+    g.add_edges_from(([(k)]))
+nx.draw(g, with_labels=True)
+
+# Раскроем одну из нод
+mixtures_subset = mixtures.sort_values('main0', ascending=False).head(1000)
+
+subset_df = df.join(mixtures_subset)
+
+autotm_subs = AutoTM(
+        topic_count=6,
+        texts_column_name='text',
+        preprocessing_params={
+            "lang": "ru",
+        },
+        alg_params={
+            "num_iterations": 10,
+            "surrogate_alg_name": "GPR"
+        },
+        working_dir_path=working_dir_path
+    )
+
+autotm_subs.print_topics()
+subs_labels = {
+    'main0': 'Технологии',
+    'main1': 'Разработка',
+    'main2': 'Игры',
+    'main3': 'Компьютеры',
+    'main4': 'Мобильные',
+    'main5': 'Переферия'
+}
+
+autotm_subs.fit_predict(subset_df)
+res_dict_subs, nodes_subs = build_graph(autotm_subs, topic_labels=subs_labels)
+
+g = nx.DiGraph()
+g.add_nodes_from(nodes_subs)
+for k, v in res_dict_subs.items():
     g.add_edges_from(([(k)]))
 nx.draw(g, with_labels=True)
